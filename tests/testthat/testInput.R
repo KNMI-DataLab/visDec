b2b12ba304dff9a2d6cf44c6b5e7ebd22d0487d2
MartinRoth <marsianus@gmail.com>
2016-05-24 19:17:00 +0200

    Grouped expectations into unit tests

--- tests/testthat/testInput.R
+++ tests/testthat/testInput.R
@@ -4,15 +4,19 @@ midnightFile <- "./Meetterrein_20151009_0000.jpg"
 standardFile <- "./Meetterrein_20151009_0610.jpg"
 sensorTestFile <- "./MOR_DeBilt_201510.txt"
 
-expect_match(FileNameParser(midnightFile, "na*me_yyyymmdd_hhmm.jpg")$name, "Meetterrein")
-expect_match(FileNameParser(standardFile, "na*me_yyyymmdd_hhmm.jpg")$dateTime, "2015-10-09 06:10:00")
+test_that("FileNameParser", {
+  expect_match(FileNameParser(midnightFile, "na*me_yyyymmdd_hhmm.jpg")$name, "Meetterrein")
+  expect_match(FileNameParser(standardFile, "na*me_yyyymmdd_hhmm.jpg")$dateTime, "2015-10-09 06:10:00")
 
-expect_error(ExtractBasicImageStatistics("bliblablub"), "File does not exist.")
+  expect_error(ExtractBasicImageStatistics("bliblablub"), "File does not exist.")
+})
 
-expect_equal_to_reference(ExtractBasicImageStatistics(midnightFile), "midnightOutput.rds")
-expect_equal_to_reference(ExtractBasicImageStatistics(standardFile), "standardOutput.rds")
+test_that("Regression tests",{
+  expect_equal_to_reference(ExtractBasicImageStatistics(midnightFile), "midnightOutput.rds")
+  expect_equal_to_reference(ExtractBasicImageStatistics(standardFile), "standardOutput.rds")
 
-expect_equal_to_reference(ReadMORSensorData(sensorTestFile), "sensorOutput.rds")
+  expect_equal_to_reference(ReadMORSensorData(sensorTestFile), "sensorOutput.rds")
+})
 
 #expect_true(testRcode())
 
