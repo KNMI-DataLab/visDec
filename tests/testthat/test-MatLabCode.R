f92b45b73fe5080eb6d6285c1584288afa1b0405
andreapago <andrea.pago@gmail.com>
2016-06-10 14:32:21 +0200

    Several bug fixed and test completing versus output of Matlab functions. Added also images and .mat files required.

--- tests/testthat/test-MatLabCode.R
+++ tests/testthat/test-MatLabCode.R
@@ -2,22 +2,42 @@ context("MatLabCode")
 
 
 test_that("Same input same output", {
-  im <- imager::load.image("./forest.jpg")
-  im <- imager::imresize(im, scale = 0.4)
+  im <- imager::load.image("./forestResizedImage.jpg")
+
+  matObjLoad <- R.matlab::readMat("../../inst/extdata/MatlabFiles/test/resizedPicture/dataResizedPicture.mat")
+
+
   winSize <- 15
+
   darkChannel <- GetDarkChannel(im, winSize)
   expect_equal_to_reference(darkChannel, "darkChannelReference.rds")
+  matrixMatLoadDarkChannel <- matObjLoad$darkChannel
+  expect_equal(darkChannel, matrixMatLoadDarkChannel)
+
   atmosphere <- GetAtmosphere(im, darkChannel)
   expect_equal_to_reference(atmosphere, "atmosphereReference.rds")
+  matrixMatLoadAtmosphere <- matObjLoad$atmosphere
+  expect_equal(atmosphere, matrixMatLoadAtmosphere)
+
+
   omega <- 0.95
   transmissionEst <- GetTransmissionEstimate(im, atmosphere, omega, winSize)
   expect_equal_to_reference(transmissionEst, "transmissionEstReference.rds")
-  # radiance <- GetRadiance(im, transmissionEst, atmosphere)
-  # expect_equal_to_reference(radiance, "radianceReference.rds")
-  # m <- imager::width(im)
-  # n <- imager::height(im)
-  # trimapAll <- array(0, c(m, n, 1, 3))
-  # laplacian <- GetLaplacian(im, trimapAll)
+  matrixMatLoadTransmission <- matObjLoad$transmission
+  expect_equal(transmissionEst, matrixMatLoadTransmission)
+
+
+  radiance <- GetRadiance(im, transmissionEst, atmosphere)
+  expect_equal_to_reference(radiance, "radianceReference.rds")
+  matrixMatLoadRadiance <- matObjLoad$radiance
+  #drop the temporal (z) dimension
+  expect_equal(drop(radiance), matrixMatLoadRadiance)
+
+
+  n <- imager::width(im)
+  m <- imager::height(im)
+  trimapAll <- array(0, c(n, m, 1, 3))#n and m inverted due to cimg representation
+  laplacian <- GetLaplacian(im, trimapAll)
   # expect_equal_to_reference(laplacian, "laplacianReference.rds")
   #lambda <- 0.0001
   #dehaze <- Dehaze(im, omega, winSize,lambda)
