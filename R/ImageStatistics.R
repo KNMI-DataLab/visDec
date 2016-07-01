7a3d7d062f7d200756bc70b30d2cee4dba90b7ff
andreapago <andrea.pago@gmail.com>
2016-07-01 11:59:35 +0200

    Do some of the functionality outside visDec

--- R/ImageStatistics.R
+++ R/ImageStatistics.R
@@ -4,12 +4,12 @@
 #' @export
 FileNameParser <- function(fullFilename, pattern="na*me_yyyymmdd_hhmm.jpg") {
   if (!file.exists(fullFilename)) stop("File does not exist.")
-  if (pattern != "na*me_yyyymmdd_hhmm.jpg" & pattern != "na*me_yyyymmddhhmm.jpg") stop("pattern not implemented")
+  if (pattern != "na*me_yyyymmdd_hhmm.jpg") stop("pattern not implemented")
   tmp <- strsplit(fullFilename, "/")[[1]]
   tmp <- tmp[length(tmp)]
   name <- tmp
   tmp <- strsplit(tmp, "_")[[1]]
-  if (length(tmp) == 3){ 
+  if (length(tmp)!=3) stop("filename does not match specified pattern")
   date <- tmp[2]
   time <- substr(tmp[3], 1, 4)
   year <- substr(date,1,4)
@@ -17,19 +17,8 @@ FileNameParser <- function(fullFilename, pattern="na*me_yyyymmdd_hhmm.jpg") {
   day   <- substr(date, 7, 8)
   hour  <- substr(time, 1, 2)
   min   <- substr(time, 3, 4)
-  }
-  else if (length(tmp) == 2) {
-    date <- substr(tmp[2], 1, 8)
-    time <- substr(tmp[2], 9, 12)
-    year <- substr(date,1,4)
-    month <- substr(date, 5, 6)
-    day   <- substr(date, 7, 8)
-    hour  <- substr(time, 1, 2)
-    min   <- substr(time, 3, 4)
-  }
-  else stop("filename does not match specified pattern")
   dateTime <- as.POSIXct(paste(paste(year,month,day,sep="-"),
                                paste(hour,min,sep=":")),
                          tz = "CET")
-  return(data.frame(name = name, dateTime = dateTime))
+  return(list(name = name, dateTime = dateTime))
 }
