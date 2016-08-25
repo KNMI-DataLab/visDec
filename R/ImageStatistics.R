a2d3864ec015a85e729f9cb345a28f2f3072d5b4
andreapago <andrea.pago@gmail.com>
2016-08-25 13:40:35 +0200

    removed non-core parts for VisDec file parsing

--- R/ImageStatistics.R
+++ R/ImageStatistics.R
@@ -4,7 +4,7 @@
 #' @export
 FileNameParser <- function(fullFilename, pattern) {
   if (!file.exists(fullFilename)) stop("File does not exist.")
-  if (pattern != "na*me_yyyymmdd_hhmm.jpg" & pattern != "na*me_yyyymmddhhmm.jpg") stop("pattern not implemented")
+  if (pattern != "na*me_yyyymmdd_hhmm.jpg") stop("pattern not implemented")
   tmp <- strsplit(fullFilename, "/")[[1]]
   tmp <- tmp[length(tmp)]
   name <- tmp
@@ -13,16 +13,6 @@ FileNameParser <- function(fullFilename, pattern) {
     filePrefix <- tmp[1]
     date <- tmp[2]
     time <- substr(tmp[3], 1, 4)
-    year <- substr(date,1,4)
-    month <- substr(date, 5, 6)
-    day   <- substr(date, 7, 8)
-    hour  <- substr(time, 1, 2)
-    min   <- substr(time, 3, 4)
-  }
-  else if (length(tmp) == 2) {
-    filePrefix <- tmp[1]
-    date <- substr(tmp[2], 1, 8)
-    time <- substr(tmp[2], 9, 12)
     year <- substr(date,1,4)
     month <- substr(date, 5, 6)
     day   <- substr(date, 7, 8)
