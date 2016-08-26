3a42425a55eb427add2a0db36e6adb3686a3cba8
andreapago <andrea.pago@gmail.com>
2016-08-26 14:39:44 +0200

    fixed timezone in sun related items implemented filtering of timewindw around sunrise/sunset

--- tests/testthat/test-sunshine.R
+++ tests/testthat/test-sunshine.R
@@ -20,7 +20,21 @@ test_that("check day", {
   #uniqueDaysStation <- UniqueDaysAndStation(imageSummary)
   #mergedStationDays <- MergeDaysWithStationConfig(uniqueDaysStation, properties)
   #print(FilterDayLightHours(mergedStationDays, imageSummary))
-  tmp <- FilterDayLightHours(fileInfo, properties)
-  expect_equal(tmp$isDay, c(FALSE, FALSE, TRUE))
+  tmp <- FilterDayLightHours(fileInfo, properties, 0, 0)
+  expect_equal(tmp$isDay, c(FALSE, TRUE, TRUE))
   expect_equal_to_reference(tmp, file = "./Reference/DaylightHours.rds")
+
 })
+
+test_that("check time window", {
+  fileInfo <- data.table::rbindlist(lapply(files, FileNameParser,
+                                           pattern="na*me_yyyymmdd_hhmm.jpg"))
+  tmp2 <- WindowFilterDayLightHours(fileInfo, properties, 120, 60, 60, 60)
+  #for the day and location the sunrise is at 05:53:02 UTC
+  #for the day the and location sunset is at 16:59:18 UTC
+  expect_equal(tmp2$toAnalyze, c(FALSE, TRUE, FALSE))
+  expect_equal_to_reference(tmp2, file = "./Reference/WindowHours.rds")
+
+}
+
+)
