c15efd5f48b03854fa4f67831c926f49341c138b
MartinRoth <marsianus@gmail.com>
2016-11-23 21:15:22 +0100

    Included fractal dimension for averaged transmission and the grayscale picture

--- R/imports.R
+++ R/imports.R
@@ -1,8 +1,9 @@
 #' @import data.table
 #' @importFrom magrittr "%>%"
-#' @importFrom imager isoblur imgradient add imsplit
+#' @importFrom imager isoblur imgradient add imsplit grayscale
 #' @importFrom plyr llply
 #' @importFrom changepoint cpt.mean cpts
 #' @importFrom stats sd
 #' @importFrom utils read.csv
+#' @importFrom fractaldim fd.estimate
 NULL
