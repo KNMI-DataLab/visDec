a651e2a28181395bbad8db1c5b22985ac5e70811
MartinRoth <marsianus@gmail.com>
2016-11-25 21:35:58 +0100

    Updated sun related stuff

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,11 @@
 .onLoad <- function(libname, pkgname) {
-  assign("oldCartesian", getOption("datatable.allow.cartesian"),
-        envir=.BaseNamespaceEnv)
-  options("datatable.allow.cartesian" = TRUE)
+  message(paste0("This is visDec version=", packageVersion("visDec")))
+  # assign("oldCartesian", getOption("datatable.allow.cartesian"),
+  #       envir=.BaseNamespaceEnv)
+  # options("datatable.allow.cartesian" = TRUE)
 }
 
 .onUnload <- function(libname, pkgname) {
-  options("datatable.allow.cartesian" = get("oldCartesian",
-        envir=.BaseNamespaceEnv))
+  # options("datatable.allow.cartesian" = get("oldCartesian",
+  #       envir=.BaseNamespaceEnv))
 }
