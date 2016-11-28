b19dd4d8bdfa11b2c7e5d22a5745a029d978dea9
MartinRoth <marsianus@gmail.com>
2016-11-28 13:54:06 +0100

    Additional test for GetFractalDim

--- tests/testthat/testDehazing.R
+++ tests/testthat/testDehazing.R
@@ -50,6 +50,7 @@ test_that("Same input same output", {
   expect_equal_to_reference(avgHorizTrans, "./Reference/avrHorizTransReference.rds")
   expect_equal_to_reference(GetFractalDim(avgHorizTrans), "./Reference/fractalDimTransmission.rds")
   expect_equal_to_reference(GetFractalDim(im), "./Reference/fractalDimImage.rds")
+  expect_error(GetFractalDim("im"), "not defined for object of class", fixed = TRUE)
 
   expect_equal_to_reference(TransmissionChangepoint(avgHorizTrans), "./Reference/transmissionChangepoint.rds")
   expect_equal_to_reference(TransmissionSmoothness(avgHorizTrans), "./Reference/transmissionSmoothness.rds")
